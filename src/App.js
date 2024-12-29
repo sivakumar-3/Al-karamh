6d5b64487eae59b9bda2a49e9f585c6c53b096c2
Navin29-4-5 <navin11104@gmail.com>
2024-12-29 23:42:25 +0530

    changes to footer on links

--- src/App.js
+++ src/App.js
@@ -4,11 +4,13 @@ import Products from './pages/products';
 import ContactPage from './pages/contactus';
 import './App.css'; // Import your custom CSS
 import { LanguageProvider } from './components/LanguageContext';
+import ScrollToTop from './components/ScrollToTop';
 function App() {
   return (
     <div className="josefin-sans">
       <LanguageProvider>
       <Router>
+      <ScrollToTop />
         <Routes>
           <Route path="/" element={<HomePage />} />
           <Route path="/products" element={<Products />} />
