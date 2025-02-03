7d2df469506609a13afee476513c4cdb517a4671
m-sowraj <140877454+m-sowraj@users.noreply.github.com>
2025-02-03 20:23:48 +0530

    mobile responsive

--- src/components/home/welcome.js
+++ src/components/home/welcome.js
@@ -20,16 +20,16 @@ export default function Welcome() {
     <div className="max-w-6xl mx-auto p-4">
       {/* Image Section */}
       <div className="flex justify-center mb-6">
-        <div className="flex gap-4">
+        <div className="flex flex-col md:flex-row gap-4">
           <img
             src="/images/products/md_pic.jpg"
             alt="MD picture"
-            className="w-auto max-w-full rounded-md shadow-md h-[450px]"
+            className="w-full md:w-auto max-w-full rounded-md shadow-md h-auto md:h-[450px]"
           />
           <img
             src="/images/products/siblings_image.jpg"
             alt="siblings picture"
-            className="w-auto max-w-full rounded-md shadow-md h-[450px]"
+            className="w-full md:w-auto max-w-full rounded-md shadow-md h-auto md:h-[450px]"
           />
         </div>
       </div>
